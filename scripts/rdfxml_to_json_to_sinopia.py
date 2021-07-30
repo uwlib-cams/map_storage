1ee33cf484bd6f86a4d4d8ecdb58e6310d738619
mcm104 <mcm104@uw.edu>
2021-07-30 11:29:45 -0700

    Convert RTs to JSON and post to Sinopia

---
+++ scripts/rdfxml_to_json_to_sinopia.py
@@ -0,0 +1,11 @@
+"""Convert RDF/XML to JSON-LD"""
+
+import rdfxml_to_json
+
+"""Edit JSON-LD for upload to Sinopia"""
+
+import edit_json
+
+"""Post edited JSON-LD to Sinopia"""
+
+import json_to_sinopia
