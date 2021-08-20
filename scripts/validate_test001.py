abc8be00f7150e01c006a19c9c46a516aa117e4a
briesenberg07 <ries07@uw>
2021-08-20 14:21:50 -0700

    delete attempt to invoke validation from Python, Saxon-HE (free) does not support XSD validation

--- scripts/validate_test001.py
+++
@@ -1,7 +0,0 @@
-# Validate XML source against schema
-import os
-os.system("java -cp ~/ries/saxon/saxon-he-10.3.jar net.sf.saxon.Validate ../map_storage_test002.xml")
-# NOT OK
-
-# TO DO:
-    # Get it to work
