fb8976450b1ceaf8c2055465433e91b4a146c765
briesenberg07 <ries07@uw>
2021-12-11 19:57:09 -0800

    move to sinopia_maps

--- scripts/saxon_he_from_py.py
+++
@@ -1,8 +0,0 @@
-# Run XSLT
-import os
-os.system("java -cp ~/ries/saxon/saxon-he-10.3.jar net.sf.saxon.Transform -t -s:build_update_test002.xsl -xsl:build_update_test002.xsl")
-# OK
-
-# TO-DO:
-    # Save output as log file
-    # (...much more)
