b5db38aaa651fe6f30736b638f2f8cbf7c19a94c
paynec4 <48925631+cspayne@users.noreply.github.com>
2023-01-30 14:39:09 -0800

    updates working, storage working

--- py/prop_storage.py
+++ py/prop_storage.py
@@ -1,3 +1,6 @@
+# This is a class created to store data related to properties 
+# retrieved from xml files for updates 
+
 class Prop:
 
     def __init__(self, id):
@@ -8,19 +11,19 @@ class Prop:
         self.prop_domain = ""
         self.implementationset = ""
 
-    def setProp_iri(self, prop_iri):
+    def set_prop_iri(self, prop_iri):
         self.prop_iri = prop_iri
     
-    def setProp_label(self, prop_label):
+    def set_prop_label(self, prop_label):
         self.prop_label = prop_label
 
-    def setProp_domain(self, prop_domain):
+    def set_prop_domain(self, prop_domain):
         self.prop_domain = prop_domain
     
-    def setImplementation_set(self, implementation_set):
+    def set_implementation_set(self, implementation_set):
         self.implementationset = implementation_set
 
-    def printProp(self):
+    def print_prop(self):
         print(
             '<prop localid_prop="'+self.id+'">\n'+
             '\t'+self.prop_iri+
--- py/update_prop_sets.py
+++ py/update_prop_sets.py
@@ -1,47 +1,96 @@
-from prop_storage import Prop
+# Written by Cypress Payne - 1/2023
+# This program is designed to update map_storage prop_sets based on
+# RDA updates by storing the current data, updating the xml documents
+# using xslt, and then comparing them to save deprecated properties
+# that contain implementation sets
+
 import xml.etree.ElementTree as ET 
 import re
+import os
+from textwrap import dedent
+#class to store properties 
+from prop_storage import Prop
+from store_props import store_props 
+# just for testing
 import sys
 
-p70k = []
+# get current xml files from path
+path = './'
+file_list = os.listdir(path)
+file_list = [ elem for elem in file_list if (elem.startswith('prop_set_test'))]
+#file_list = [ elem for elem in file_list if (elem.endswith('.xml') 
+#    and (elem.startswith('prop_set_rd') or elem.startswith('prop_set_uw')))]
 
-def RemoveNameSpace(xmlString): 
-    re_ns = ' xmlns="[^ ]*'
-    re_uwns = ' xmlns[^>]*'
-    re_xml = "<\?xml version='1\.0' encoding='utf8'\?>\n"
+#create dictionary of arrays for each xml file
+file_dict = {}
+for f in file_list:
+    file_dict[f] = []
 
-    xmlString = re.sub(re_ns, '', xmlString)
-    xmlString = re.sub(re_uwns, '', xmlString)
-    xmlString = re.sub(re_xml, '', xmlString)
-    return xmlString
+print(file_dict)
+#for each file, store current properties in array 
+store_props(file_dict)
+print(file_dict)
 
-tree = ET.parse('./prop_set_rdat_p70k.xml')
-ET.register_namespace('', "https://uwlib-cams.github.io/map_storage/xsd/")
-ET.register_namespace('dcam', "http://purl.org/dc/dcam/")
-ET.register_namespace('owl', "http://www.w3.org/2002/07/owl#")
-ET.register_namespace('prov', "http://www.w3.org/ns/prov#")
-ET.register_namespace('rdf', "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
-ET.register_namespace('rdfs', "http://www.w3.org/2000/01/rdf-schema#")
-ET.register_namespace('reg', "http://metadataregistry.org/uri/profile/regap/")
-ET.register_namespace('uwsinopia', "https://uwlib-cams.github.io/sinopia_maps/xsd/")
-ET.register_namespace('xs', "http://www.w3.org/2001/XMLSchema")
-ET.register_namespace('xsi', "http://www.w3.org/2001/XMLSchema-instance")
+# i = file_dict.get("prop_set_rdaa_p50k.xml")
+# i[0].print_prop()
 
-root = tree.getroot()
+"""Setup and run XSLT transformation"""
 
-for prop in root.iter('{https://uwlib-cams.github.io/map_storage/xsd/}prop'):
-    newProp = Prop(prop.attrib['localid_prop'])
-    for child in prop.iter():
-        if child.tag == "{https://uwlib-cams.github.io/map_storage/xsd/}prop_iri":
-            newProp.setProp_iri(RemoveNameSpace(ET.tostring(child, encoding='utf8').decode('utf8')))
-        if child.tag == "{https://uwlib-cams.github.io/map_storage/xsd/}prop_label":
-            newProp.setProp_label(RemoveNameSpace(ET.tostring(child, encoding='utf8').decode('utf8')))
-        if child.tag == "{https://uwlib-cams.github.io/map_storage/xsd/}prop_domain":
-            newProp.setProp_domain(RemoveNameSpace(ET.tostring(child, encoding='utf8').decode('utf8')))
-        if child.tag == "{https://uwlib-cams.github.io/map_storage/xsd/}sinopia":
-            newProp.setImplementation_set(RemoveNameSpace(ET.tostring(child, encoding='utf8').decode('utf8')))
-    p70k.append(newProp)
+saxon_dir_prompt = dedent("""Enter the name of the directory in which the Saxon HE .jar file is stored
+For example: 'saxon', 'saxon11', etc.
+> """)
+saxon_dir = input(saxon_dir_prompt)
 
-print(len(p70k))
+saxon_version_prompt = dedent("""
+Enter the Saxon HE version number you'll use for the transformation
+For example: '11.1', '11.4', etc.
+> """)
+saxon_version = input(saxon_version_prompt)
 
-p70k[0].printProp()
+os.system(f'java -cp ~/{saxon_dir}/saxon-he-{saxon_version}.jar net.sf.saxon.Transform -t -s:xml/get_prop_sets.xml -xsl:xsl/001_01_build_update.xsl')
+
+new_file_list = os.listdir(path)
+
+new_file_list = [ elem for elem in file_list if (elem.startswith('prop_set_test'))]
+## not using dcterms or prov
+# new_file_list = [ elem for elem in new_file_list if (elem.endswith('.xml') 
+#     and (elem.startswith('prop_set_rd') or elem.startswith('prop_set_uw')))]
+
+print(new_file_list)
+#create dictionary of arrays for each xml file
+new_file_dict = {}
+for f in new_file_list:
+    new_file_dict[f] = []
+
+print(new_file_dict)
+#for each file, store current properties in array 
+store_props(new_file_dict)
+print(new_file_dict)
+
+print(sys.getsizeof(file_dict))
+print(sys.getsizeof(new_file_dict))
+# del(file_dict)
+
+# for key in file_dict.keys():
+  #  array1 = file_dict.get(key)
+   # for new_key in new_file_dict.keys():
+      #  array2 = new_file_dict.get(key)
+
+array1 = file_dict.get("prop_set_test.xml")
+array2 = new_file_dict.get("prop_set_test1.xml")
+deprecated_props = []
+
+for index1, i in enumerate(array1):
+    #print(i.prop_iri)
+    # print("index1: ")
+    # print(index1)
+    length = len(array2)
+    print(length)
+    for index2, j in enumerate(array2):
+        if(i.prop_iri != j.prop_iri):
+            length = length - 1
+            print(length)
+    if length == 0:
+        deprecated_props.append(i)
+print(len(deprecated_props))
+deprecated_props[0].print_prop()
